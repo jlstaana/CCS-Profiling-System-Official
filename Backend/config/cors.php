6ab61ee697650732dcf2661d5e6984a7c629f49d
jlstaana <staanajulianalouise44@gmail.com>
2026-04-04 21:17:07 +0800

    feat: configure CORS settings to allow specified frontend and development origins

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -6,7 +6,18 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://predatorily-nonfelonious-ranae.ngrok-free.dev',
+        'https://ccs-profiling-presentation.vercel.app',
+        'https://ccs-profiling-system-official-frontend.vercel.app',
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+        'http://localhost:5176',
+        'http://localhost:5177',
+        'http://127.0.0.1:5173',
+        'http://127.0.0.1:8000',
+    ],
 
     'allowed_origins_patterns' => [],
 
